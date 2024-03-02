48b6ded0c56297e460b9120d2fb9667cdb5e7a27
nefeygt <n.efeyigit@outlook.com>
2024-03-02 17:23:27 +0300

    added level 3

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int cherries = 0;
+    private int fruits = 0;
 
-    [SerializeField] private Text cherryText;
+    [SerializeField] private Text fruitText;
 
     [SerializeField] private AudioSource collectionSoundEffect;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Cherry"))
+        if(collision.gameObject.CompareTag("Fruit"))
         {
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
-            cherries++;
-            cherryText.text = "Cherries: " + cherries;
+            fruits++;
+            fruitText.text = "Fruits: " + fruits;
         }
     }
 }
